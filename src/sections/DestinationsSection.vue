ce83b0fa7b73a246889c5ab53c484fcfdec01101
Gideon Wanjohi <gideon@stickman.corp>
2023-01-22 18:41:16 +0300

    Update destinations carousel | Add backround color

--- src/sections/DestinationsSection.vue
+++ src/sections/DestinationsSection.vue
@@ -19,14 +19,19 @@
       >
         <v-carousel-item v-for="slide in items" :key="slide.title">
           <div
-            class="d-flex justify-space-between align-center flex-column flex-md-row"
+            class="d-flex justify-space-between align-center flex-column flex-md-row py-7 px-7 mb-2"
+            :style="{ background: slide.bg }"
           >
-            <v-card
+            <div
               flat
               class="d-flex justify-space-between align-center"
-              max-width="400"
+              style="
+                 {
+                  max-width: 400px;
+                }
+              "
             >
-              <div>
+              <div class="mr-md-3">
                 <p class="pacifico-text text-center font-weight-black text-h2">
                   {{ slide.title }}
                 </p>
@@ -34,14 +39,13 @@
                   {{ slide.about }}
                 </p>
               </div>
-            </v-card>
-            <v-card color="white" elevation="10" class="mt-5 mr-7">
+            </div>
+            <v-card color="white" elevation="13" class="mt-5 mr-7">
               <v-img
                 :src="slide.src"
                 max-height="300"
                 max-width="500"
                 class="tilted-left"
-                elevation="12"
               />
             </v-card>
           </div>
@@ -59,31 +63,36 @@ export default {
       "https://images.unsplash.com/photo-1445019980597-93fa8acb246c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     items: [
       {
-        src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        bg: "#94b6cf1c",
+        src: "https://www.tripitaca.com/_nuxt/img/Mombasa.baaeaab.jpeg",
         title: "Mombasa",
         about:
-          "Mombasa is the number one tourist destination in Kenya. It is situated on the Eastern coastline of Kenya next to the Indian Ocean and it is famous among tourists for its spectacular beaches. Mombasa has an assorted aquatic life, luxurious hotels as well as a very hospitable environment. Mombasa has more...",
+          "Mombasa is the number one tourist destination in Kenya. It is situated on the Eastern coastline of Kenya next to the Indian Ocean and it is famous among tourists for its spectacular beaches. Mombasa has an assorted aquatic life, luxurious hotels as well as a very hospitable environment.",
       },
       {
-        src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        bg: "#eeedfd52",
+        src: "https://www.tripitaca.com/_nuxt/img/Kisumu.ddd473b.jpeg",
         title: "Kisumu",
         about:
-          "Kisumu is a Kenyan port city on Lake Victoria (the second largest fresh water lake in the world). It is the third largest city in Kenya, a fact that its locals take great proud in. (The other fact we take great pride in is Barack Obama, considered by all to be a native son). Kisumu is w more...",
+          "Kisumu is a Kenyan port city on Lake Victoria (the second largest fresh water lake in the world). It is the third largest city in Kenya, a fact that its locals take great proud in. (The other fact we take great pride in is Barack Obama, considered by all to be a native son).",
       },
       {
-        src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        bg: "#743c370d",
+        src: "https://www.tripitaca.com/_nuxt/img/Naivasha.9f02dc7.jpeg",
         title: "Naivasha",
         about:
-          "Lake Naivasha derives its name from the Maasai word for ‘rough water’ due to the sudden storms that are known to arise in the area. The lake is a refreshing day trip or weekend break from Kenya’s busy capital. Lake Naivasha is famous for its strong populations of hippos and flamingos. Th more...",
+          "Lake Naivasha derives its name from the Maasai word for ‘rough water’ due to the sudden storms that are known to arise in the area. The lake is a refreshing day trip or weekend break from Kenya’s busy capital. Lake Naivasha is famous for its strong populations of hippos and flamingos.",
       },
       {
-        src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+        bg: "#66575214",
+        src: "https://www.tripitaca.com/_nuxt/img/Nanyuki.d09177c.jpeg",
         title: "Nanyuki",
         about:
           "Nanyuki (meaning 'place of red water' in the Masai language) is a town in Laikipia County of Kenya lying northwest of Mount Kenya (the second highest mountain in Africa). It was founded in 1907 by British settlers, some of whose descendants still live in and around the town. Nanyuki has several wild more...",
       },
       {
-        src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        bg: "#cfa53917",
+        src: "https://www.tripitaca.com/_nuxt/img/Nairobi.073643e.jpeg",
         title: "Nairobi",
         about:
           "Nairobi is the second famous tourist destination in Kenya. It is situated on the Eastern coastline of Kenya next to the Indian Ocean and it is famous among tourists for its spectacular beaches. Mombasa has an assorted aquatic life, luxurious hotels as well as a very hospitable environment. Mombasa has more...",
